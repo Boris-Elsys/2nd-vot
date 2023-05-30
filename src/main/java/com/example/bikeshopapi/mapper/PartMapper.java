ad7009afaf3937ce34229bdef90782c9ed0ba27c
Elsys-Boris <elsys.boris@gmail.com>
2023-05-30 22:48:37 +0300

    Part Mapper

--- src/main/java/com/example/bikeshopapi/mapper/PartMapper.java
+++ src/main/java/com/example/bikeshopapi/mapper/PartMapper.java
@@ -1,4 +1,20 @@
 package com.example.bikeshopapi.mapper;
 
+import com.example.bikeshopapi.controller.resources.PartResource;
+import com.example.bikeshopapi.entity.Part;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(uses = {BikeShopMapper.class})
 public interface PartMapper {
+    public PartMapper MECHANIC_MAPPER = Mappers.getMapper(PartMapper.class);
+
+    Part fromPartResource(PartResource resource);
+
+    PartResource toPartResources(Part part);
+
+    List<PartResource> toPartResources(List<PartMapper> parts);
+
 }
