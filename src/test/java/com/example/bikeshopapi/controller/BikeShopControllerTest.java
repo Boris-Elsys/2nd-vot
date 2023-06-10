e80db47eefd646550d586d9b9d0ed08396412799
Elsys-Boris <elsys.boris@gmail.com>
2023-06-10 17:15:09 +0300

    BikeShop Test

---
+++ src/test/java/com/example/bikeshopapi/controller/BikeShopControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.bikeshopapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BikeShopControllerTest {
+
+    @Test
+    @ExtendWith(MockitoExtension.class)
+    void getAll() {
+
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
