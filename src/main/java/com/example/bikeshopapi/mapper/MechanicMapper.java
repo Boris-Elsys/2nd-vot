d266d15218a107480ea04134f0a6984d23e5ac9f
Elsys-Boris <elsys.boris@gmail.com>
2023-06-10 23:55:15 +0300

    mech map

--- src/main/java/com/example/bikeshopapi/mapper/MechanicMapper.java
+++ src/main/java/com/example/bikeshopapi/mapper/MechanicMapper.java
@@ -8,14 +8,14 @@ import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(uses = {BikeShopMapper.class})
+@Mapper
 public interface MechanicMapper {
     public MechanicMapper MECHANIC_MAPPER = Mappers.getMapper(MechanicMapper.class);
     @Mapping(source = "bikeShop.name", target = "bikeShopName")
-    Mechanic fromMechanicResource(MechanicResource resource);
-
-    @Mapping(source = "bikeShopName", target = "bikeShop.name")
     MechanicResource toMechanicResource(Mechanic mechanic);
+
+    @Mapping(source = "bikeShopName", target = "bikeShop.name")//flipped
+    Mechanic fromMechanicResource(MechanicResource resource);
     List<MechanicResource> toMechanicResources(List<Mechanic> mechanics);
 
     List<Mechanic> fromMechanicResources(List<MechanicResource> mechanicResources);
